a84dbd2b619c623fba9bc4eef27ddf8e76ab12c7
Le Huyen Bao Trang <l.h.b.t.2k3@gmail.com>
2023-11-25 13:20:31 +0700

    change6

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -1,39 +1,39 @@
 ﻿using BookShop.Data;
 using BookShop.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace BookShop.Controllers
 {
-    public class CategoryController : Controller
-    {
+	public class CategoryController : Controller
+	{
 		private readonly ApplicationDBContext _dbContext;
 		public CategoryController(ApplicationDBContext applicationDBContext)
 		{
 			_dbContext = applicationDBContext;
 		}
 		public IActionResult Index()
-        {
+		{
 			List<Category> categories = _dbContext.Categories.ToList();
 			return View(categories);
 		}
-
-        public IActionResult Details()
-        {
-            return View();
-        }
-
+		public IActionResult Details()
+		{
+			return View();
+		}
 		public IActionResult Create()
 		{
 			return View();
 		}
-
 		[HttpPost]
-
 		public IActionResult Create(Category category)
 		{
-			_dbContext.Categories.Add(category);
-			_dbContext.SaveChanges();
+
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Add(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category Created successfully";
+			}
 			return RedirectToAction("Index");
 
 		}
--- BookShop/Models/Category.cs
+++ BookShop/Models/Category.cs
@@ -2,14 +2,13 @@
 
 namespace BookShop.Models
 {
-    public class Category
-    {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int DisplayOrder { get; set; }
-
-    }
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int DisplayOrder { get; set; }
+	}
 }
